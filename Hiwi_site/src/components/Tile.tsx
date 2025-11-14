ed001f2f3f767ed4b910952250d9bca3e085b39e
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2025-11-14 17:06:29 +0000

    map verstanden

--- Hiwi_site/src/components/Tile.tsx
+++ Hiwi_site/src/components/Tile.tsx
@@ -3,12 +3,15 @@ import styles from '../styles/Tile.module.css';
 type TileProps = {
   title: string;
   link: string;
+  image: string;
 };
 
-export default function Tile({ title, link }: TileProps) { 
+export default function Tile({ title, link, image }: TileProps) { // das Tileprops weisst hier einfach den Parametern die vorher definierten Werte zu wie  title:str, link:string, image:string
   return (
+    
     <a href={link} className={styles.tile}>
-      <div>{title}</div>
+      <img src={image} alt={title}/>
+      <span>{title}</span>
     </a>
   );
 }
--- Hiwi_site/src/components/TileGrid.tsx
+++ Hiwi_site/src/components/TileGrid.tsx
@@ -1,16 +1,17 @@
 import Tile from './Tile';
 import styles from '../styles/TileGrid.module.css';
 
-const tiles = Array.from({ length: 20 }, (_, i) => ({
+const tiles = Array.from({ length: 15 }, (_, i) => ({  // das array was durchläufen wird mit array.map() und in map() kommt das objekt innerhalb des arrays rein. => map(tiles) also zusammen array.map(objekt im array)
   title: `Bild ${i + 1}`,
-  link: `/seite-${i + 1}`,
+  link: `/Seite-${i + 1}`,
+  image: `/images/bild${i + 1}.png`,
 }));
 
 export default function TileGrid() {
   return (
-    <div className={styles.grid}>
-      {tiles.map((tile) => (
-        <Tile key={tile.title} title={tile.title} link={tile.link} />
+    <div className={styles.grid}>  
+      {tiles.map((tile) => (  // starte das durchlaufenvon tile mit map (for schleife) ALSO array.map(i) => <objektname parameterkey = {i.index1} WICHTIG !!!!
+        <Tile key={tile.title} title={tile.title} link={tile.link} image={tile.image} /> // übergebe diese Parameter beim durchlauf
       ))}
     </div>
   );
